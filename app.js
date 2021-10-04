d87d64ffe8299846d12e3e65c913d1f526659613
Belig Chimitov <belig.ch111@gmail.com>
2021-10-04 19:08:17 +0700

    added starter code

---
+++ app.js
@@ -0,0 +1,43 @@
+const {Telegraf, Scenes, Markup, session} = require("telegraf")
+require("dotenv/config")
+const constants = require("./constants")
+const welcomeSceneGenerate = require("./scenes/sceneWelcome/WelcomeScene")
+
+// Bot init
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+
+// Stages init
+const stage = new Scenes.Stage([
+    welcomeSceneGenerate()
+])
+
+// Middlewares
+bot.use(Telegraf.log())
+bot.use(session())
+bot.use(stage.middleware())
+
+
+// Middleware: Check if chat type is private
+bot.use((ctx, next) => ctx.chat && ctx.chat.type !== "private" ? ctx.reply(constants.TEXT_CHAT_NOT_PRIVATE) : next())
+
+
+// Commands
+bot.start(async (ctx) => {
+    await ctx.scene.enter(constants.SCENE_ID_WELCOME)
+})
+
+bot.hears(constants.BUTTON_TEXT_HELLO, async (ctx) => {
+    await ctx.reply("Привет еще раз", {
+        reply_markup: Markup.removeKeyboard().reply_markup
+    })
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+
+
